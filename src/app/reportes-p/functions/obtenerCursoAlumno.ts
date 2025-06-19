5b0eba98ee29855f2bc43e494acb542ca79bb695
Lucho <luiscarlossomoza@gmail.com>
2025-06-18 19:23:17 -0600

    Configuración de CORS peticiones de perfil desde vista de profesor

--- src/app/reportes-p/functions/obtenerCursoAlumno.ts
+++ src/app/reportes-p/functions/obtenerCursoAlumno.ts
@@ -1,7 +1,15 @@
 
 export default async function obtenerCursoAlumno(id_grupo: number){
     const baseUrl = process.env.NEXT_PUBLIC_API_URL;
-    const datosHorario = await fetch(`${baseUrl}/grupos/curso/${id_grupo}`)
+    const datosHorario = await fetch(`${baseUrl}/grupos/curso/${id_grupo}`,{
+      method: 'GET', // Método de la solicitud (GET, POST, PUT, DELETE, etc.)
+      mode: 'cors',  // **Política CORS: 'cors' permite solicitudes a otros orígenes**
+      headers: {
+        'Content-Type': 'application/json', // Tipo de contenido que esperamos enviar/recibir
+        'Accept': 'application/json'       // Especifica el tipo de respuesta deseada
+        // 'Authorization': 'Bearer YOUR_TOKEN' // Ejemplo de cabecera de autorización, si es necesario
+      }
+    })
     const resultadoConsulta = await datosHorario.json()
     console.log(resultadoConsulta)
     return resultadoConsulta;
--- src/app/reportes-p/functions/obtenerProfesorAlumno.ts
+++ src/app/reportes-p/functions/obtenerProfesorAlumno.ts
@@ -1,5 +1,13 @@
 export default async function obtenerProfesorGrupo(id_profesor: number){
-    const datosHorario = await fetch(`http://localhost:5555/grupos/profesores/${id_profesor}`)
+    const datosHorario = await fetch(`http://localhost:5555/grupos/profesores/${id_profesor}`,{
+      method: 'GET', // Método de la solicitud (GET, POST, PUT, DELETE, etc.)
+      mode: 'cors',  // **Política CORS: 'cors' permite solicitudes a otros orígenes**
+      headers: {
+        'Content-Type': 'application/json', // Tipo de contenido que esperamos enviar/recibir
+        'Accept': 'application/json'       // Especifica el tipo de respuesta deseada
+        // 'Authorization': 'Bearer YOUR_TOKEN' // Ejemplo de cabecera de autorización, si es necesario
+      }
+    })
     const resultadoConsulta = await datosHorario.json()
     console.log(resultadoConsulta)
     return resultadoConsulta;
